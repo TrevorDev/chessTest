aaa75b3410651000a93ffbfa9d9a3068aeaecb6b
unknown <trevorjbaron@gmail.com>
2014-10-27 21:57:07 -0400

    try cukes

---
+++ features/classic/start.feature
@@ -0,0 +1,7 @@
+Feature: start
+	In order to do this
+	As a killer
+	I want to kill
+
+	Scenario: start
+		Given I have 48 cukes in my belly
---
+++ features/step_definitions/start.java
@@ -0,0 +1,4 @@
+@Given("I have (\\d+) cukes in my belly")
+public void I_have_cukes_in_my_belly(int cukes) {
+    // Do something with the cukes
+}
